43f34dd1e60848c8373e4646d2e29b07c9909285
Pablo Gutiérrez del Castillo <pablogutierrezdelc@gmail.com>
2011-11-10 17:04:23 -0300

    Added gzip support

--- smbackup.sh
+++ smbackup.sh
@@ -5,11 +5,12 @@
 # Uso: backup.sh <archivo.conf>
 #
 
-BACKUPDIR="/var/backups/pcs"
+BACKUPDIR="/home/mool/dev/smbackup/files"
 SMBCLIENT=$(which smbclient)
 
-
 if [ $1 ]; then
+  [ ! -d $BACKUPDIR ] && echo "Creando el directorio de backups" && mkdir $BACKUPDIR
+
 	CONFIG_FILE=$1
 	FECHA=$(date +%F)
 
@@ -23,25 +24,15 @@ if [ $1 ]; then
 		BACKUP_HOST=$(echo $1 | awk -F / '{ print $3 }')
 		BACKUP_SRC=$(echo $1 | awk -F / '{ print $4 }')
 
-		if [ ! -d $BACKUPDIR/$FECHA ]; then
-			echo Creando el directorio para la FECHA $FECHA
-			mkdir $BACKUPDIR/$FECHA
-		fi
-		if [ ! -d $BACKUPDIR/$FECHA/$BACKUP_HOST ]; then
-			echo Creando el directorio para el Host $BACKUP_HOST
-			mkdir $BACKUPDIR/$FECHA/$BACKUP_HOST
-		fi
+		[ ! -d $BACKUPDIR/$FECHA ] && echo "Creando el directorio para la FECHA $FECHA" && mkdir $BACKUPDIR/$FECHA
+		[ ! -d $BACKUPDIR/$FECHA/$BACKUP_HOST ] && echo "Creando el directorio para el Host $BACKUP_HOST" && mkdir $BACKUPDIR/$FECHA/$BACKUP_HOST
 
-		echo Haciendo el backup de $BACKUP_HOST - $BACKUP_SRC...
+		echo "Haciendo el backup de $BACKUP_HOST - $BACKUP_SRC..."
 
 		if [ $2 ]; then
-			if [ $3 ]; then
-				$SMBCLIENT -U $2 $1 $3 -c "tarmode full" -Tcq $BACKUPDIR/$FECHA/$BACKUP_HOST/$BACKUP_SRC.tar
-			else
-				$SMBCLIENT -U $2 $1 -c "tarmode full" -Tcq $BACKUPDIR/$FECHA/$BACKUP_HOST/$BACKUP_SRC.tar
-			fi
+				$SMBCLIENT -U $2 $1 $3 -c "tarmode full" -Tcq - | gzip > $BACKUPDIR/$FECHA/$BACKUP_HOST/$BACKUP_SRC.tar.gz
 		else
-			$SMBCLIENT $1 -N -c "tarmode full" -Tcq $BACKUPDIR/$FECHA/$BACKUP_HOST/$BACKUP_SRC.tar
+			$SMBCLIENT $1 -N -c "tarmode full" -Tcq - | gzip > $BACKUPDIR/$FECHA/$BACKUP_HOST/$BACKUP_SRC.tar.gz
 		fi
 	done < $CONFIG_FILE
 else
