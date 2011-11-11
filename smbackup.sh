1803113a0ec1dc9a8bff4345e8113ddc80e95c8b
Pablo Gutiérrez del Castillo <pablogutierrezdelc@gmail.com>
2011-11-11 17:17:01 -0300

    Backup directory configuration in the config file

--- smbackup.sh
+++ smbackup.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-## Script de backup de recursos compartidos SAMBA
+## Script de backup de recursos compartidos Windows/SAMBA
 #
 # Uso: backup.sh <archivo.conf>
 #
 
-BACKUPDIR="/home/mool/dev/smbackup/files"
+BACKUPDIR="/var/backups/pcs"
 SMBCLIENT=$(which smbclient)
 
 if [ $1 ]; then
-  [ ! -d $BACKUPDIR ] && echo "Creando el directorio de backups" && mkdir $BACKUPDIR
-
 	CONFIG_FILE=$1
 	FECHA=$(date +%F)
 
@@ -20,17 +18,23 @@ if [ $1 ]; then
 		[[ ${line:0:1} == "#" ]] && continue
 		# skip empty lines
 		[[ -z "$line" ]] && continue
+		# configuration
+		if [[ ${line:0:9} == "BACKUPDIR" ]]; then
+			BACKUPDIR=$(echo $line | awk -F = '{ print $2 }')
+			[ ! -d $BACKUPDIR ] && echo -e "Creando el directorio de backups\n" && mkdir $BACKUPDIR
+			continue
+		fi
 
 		BACKUP_HOST=$(echo $1 | awk -F / '{ print $3 }')
 		BACKUP_SRC=$(echo $1 | awk -F / '{ print $4 }')
 
 		[ ! -d $BACKUPDIR/$FECHA ] && echo "Creando el directorio para la FECHA $FECHA" && mkdir $BACKUPDIR/$FECHA
-		[ ! -d $BACKUPDIR/$FECHA/$BACKUP_HOST ] && echo "Creando el directorio para el Host $BACKUP_HOST" && mkdir $BACKUPDIR/$FECHA/$BACKUP_HOST
+		[ ! -d $BACKUPDIR/$FECHA/$BACKUP_HOST ] && echo -e "\nCreando el directorio para el Host $BACKUP_HOST" && mkdir $BACKUPDIR/$FECHA/$BACKUP_HOST
 
 		echo "Haciendo el backup de $BACKUP_HOST - $BACKUP_SRC..."
 
 		if [ $2 ]; then
-				$SMBCLIENT -U $2 $1 $3 -c "tarmode full" -Tcq - | gzip > $BACKUPDIR/$FECHA/$BACKUP_HOST/$BACKUP_SRC.tar.gz
+			$SMBCLIENT -U $2 $1 $3 -c "tarmode full" -Tcq - | gzip > $BACKUPDIR/$FECHA/$BACKUP_HOST/$BACKUP_SRC.tar.gz
 		else
 			$SMBCLIENT $1 -N -c "tarmode full" -Tcq - | gzip > $BACKUPDIR/$FECHA/$BACKUP_HOST/$BACKUP_SRC.tar.gz
 		fi
